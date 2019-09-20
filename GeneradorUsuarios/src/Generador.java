9dffa33f3be61eaac15821845f69ff36cbbf8e3e
tusawe <joseluisgomezlopez91@gmail.com>
2019-09-20 12:45:55 +0200

    20/09_12:45

--- GeneradorUsuarios/src/Generador.java
+++ GeneradorUsuarios/src/Generador.java
@@ -3,6 +3,7 @@ public class Generador {
     public static String[] hombres = {"Paco","Manuel", "Pedro", "José"};
     public static String[] mujeres = {"Marta", "María", "Raquel", "Lucía"};
     public static String[] apellidos = {"Martínez","Gómez","López","Moreno","Castillo","Moya","Hernández","Rubio"};
+    public static String[] emails = {""};
 
 
     public static void main() {
@@ -13,7 +14,7 @@ public class Generador {
 
     public static void generarUsuario(){
 
-        
+
 
     }
 
